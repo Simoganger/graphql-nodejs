b5df075a823543e2961c21788c691ae8c918f216
simoganger <simonsoft7@gmail.com>
2023-12-30 01:55:42 -0500

    Integrate GraphQL with NodeJS

---
+++ index.js
@@ -0,0 +1,23 @@
+const { post } = require("superagent")
+
+const query = `query {
+    viewer {
+        login
+    }
+}`;
+
+const token = process.env.GITHUB_TOKEN;
+const headers = {
+    "User-Agent": "superagent",
+    Authorization: `Bearer ${token}`
+};
+
+async function main() {
+    const { body } = await post("https://api.github.com/graphql")
+    .set(headers)
+    .send({ query });
+
+    console.log(body)
+}
+
+main();
